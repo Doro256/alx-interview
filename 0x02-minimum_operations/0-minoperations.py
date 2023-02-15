e595ac53e6c1c3dbc024a87aac26a71d6ff6b705
Dorothy Nantumbwe <dorothynantumbwe@yahoo.com>
2023-02-16 02:59:48 +0300

    fixed pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """  Minimum operations """
 
+
 def minOperations(n):
-    """ method that calculates the fewest number of operations needed to result in exactly n H characters in the file """
+    """ method that calculates the fewest number of operations needed
+    to result in exactly n H characters in the file """
 
     if n <= 1:
         return 0
