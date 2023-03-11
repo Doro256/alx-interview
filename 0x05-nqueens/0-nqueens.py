84bde092548c171e39047691457a058757ba281e
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-11 16:10:39 +0300

    modified:   0x05-nqueens/0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,42 +1,70 @@
 #!/usr/bin/python3
 
-import sys
+#!/usr/bin/python3
+''' Solving the N queens problem '''
+
+from sys import argv, exit
+
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+try:
+    N = int(argv[1])
+except:
+    print('N must be a number')
+    exit(1)
+
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
-def nqueens(N):
-    if not isinstance(N, int):
-        print("N must be a number")
-        sys.exit(1)
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    def is_valid(board, row, col):
-        for r, c in board:
-            if r == row or c == col or abs(r - row) == abs(c - col):
-                return False
-        return True
-
-    def backtrack(board, row):
-        if row == N:
-            print(board)
-            return
+def print_queens(board):
+    """ prints queen indexes """
+    indexes = list()
+    for row in range(N):
         for col in range(N):
-            if is_valid(board, row, col):
-                board.append([row, col])
-                backtrack(board, row + 1)
-                board.pop()
-
-    backtrack([], 0)
+            if board[col][row] == 1:
+                indexes.append([row, col])
+    print(indexes)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    nqueens(N)
+def safety(board, row, col):
+    """ determines if the queen is safe at this position """
+    if 1 in board[row]:
+        return False
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while i < N and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+def nqueens(board, col):
+    """ beginning of n queens algo"""
+    if col == N:
+        print_queens(board)
+        return True
+    res = False
+    for i in range(N):
+        if safety(board, i, col):
+            board[i][col] = 1
+            res = nqueens(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+chessboard = []
+for i in range(N):
+    chessboard.append([0]*N)
+nqueens(chessboard, 0)
