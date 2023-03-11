3309bd7f87532e74743c98c55d96dce4e1cf1464
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-11 23:16:09 +0300

    modified 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
+
+"""Script that reads stdin line by line and computes metrics"""
+
 import sys
 
-# Define possible status codes
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
-# Initialize variables for metrics
-total_size = 0
-lines_by_status_code = {code: 0 for code in status_codes}
-
-# Define function to print metrics
-
-
-def print_metrics():
-    print("Total file size: File size: {}".format(total_size))
-    for code in sorted(lines_by_status_code.keys()):
-        if lines_by_status_code[code] > 0:
-            print("{}: {}".format(code, lines_by_status_code[code]))
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-# Read from stdin line by line
-line_count = 0
-for line in sys.stdin:
-    # Parse line using input format
-    try:
-        ip_address, date, request, status_code_str, size_str = line.split(" ")
-        status_code = int(status_code_str)
-        size = int(size_str)
-    except ValueError:
-        continue
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-    # Update metrics
-    total_size += size
-    if status_code in status_codes:
-        lines_by_status_code[status_code] += 1
+count = 0
+size = 0
 
-    line_count += 1
-    # Print metrics after every 10 lines or upon keyboard interruption
-    if line_count % 10 == 0:
-        print_metrics()
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-# Print final metrics upon EOF
-print_metrics()
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except BaseException:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except BaseException:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
