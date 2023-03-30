1dfd4cec1665c6c6b8b896ba7033b2016baf49ff
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-30 20:05:15 +0300

    island_perimeter done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+def island_perimeter(grid):
+    # Initialize perimeter and dimensions of the grid
+    perimeter = 0
+    n = len(grid)
+    m = len(grid[0])
+
+    # Traverse the grid to count the perimeter
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                # Count top edge
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Count bottom edge
+                if i == n - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Count left edge
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Count right edge
+                if j == m - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
